b9f9493dcfd95c36858b5601b49bbc213080516e
chenweiyan <chanweiyan007@gmail.com>
2020-06-25 19:47:51 +0800

    export Truck instance to global env

--- scripts/main.js
+++ scripts/main.js
@@ -3,4 +3,6 @@
   var App = window.App;
   var Truck = App.Truck;
   var DataStore = App.DataStore;
+  var myTruck = new Truck('ncc-1701', new DataStore());
+  window.myTruck = myTruck;
 })(window);
